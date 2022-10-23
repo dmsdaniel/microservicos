1f145b0a33f22b883ece6b9818e4cf9f028ff65a
daniel.santos <daniel@DESKTOP-4SJQK0F.localdomain>
2022-10-22 23:55:02 -0300

    first commit

---
+++ product-service/src/main/java/com/example/productservice/dto/ProductRequest.java
@@ -0,0 +1,18 @@
+package com.example.productservice.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductRequest {
+    String nome;
+    String descricao;
+    BigDecimal preco;
+}
