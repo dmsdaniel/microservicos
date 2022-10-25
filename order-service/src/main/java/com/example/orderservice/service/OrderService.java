3248fb1be56361581ca600586351945a6bd28218
Daniel Santos <dmsdaniel@gmail.com>
2022-10-25 16:35:52 -0300

    ordem service

---
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,35 @@
+package com.example.orderservice.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.example.orderservice.dto.OrderItemsDto;
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.model.OrderItems;
+import com.example.orderservice.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderItems> list = orderRequest.getOrderItensDto().stream().map(this::mapToDto).toList();
+        order.setOrderItens(list);
+        orderRepository.save(order);
+    }
+    private OrderItems mapToDto(OrderItemsDto ordemItemDto) {
+        OrderItems orderItems = new OrderItems();
+        orderItems.setPrice(ordemItemDto.getPrice());
+        orderItems.setSkuCode(ordemItemDto.getSkuCode());
+        orderItems.setQuantity(ordemItemDto.getQuantity());
+        return orderItems;
+    }
+    
+}
--- product-service/src/main/java/com/example/productservice/dto/ProductRequest.java
+++ product-service/src/main/java/com/example/productservice/dto/ProductRequest.java
@@ -3,14 +3,14 @@ package com.example.productservice.dto;
 import java.math.BigDecimal;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Builder
+@Data
 public class ProductRequest {
     String nome;
     String descricao;
