8b43865542eb554799ff55345a15001f73a960fe
Daniel Santos <dmsdaniel@gmail.com>
2022-10-26 22:46:19 -0300

    apagando pastas antigas

--- product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.productservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.productservice.model.Product;
-
-public interface  ProductRepository extends MongoRepository<Product, String> {
-    
-}
