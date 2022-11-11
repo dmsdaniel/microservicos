18514b4ee724992a52612f52f46667b1105ea1d3
daniel santos <daniel@wareline.com.br>
2022-11-10 21:27:51 -0300

    distributed-tracing

--- microservices-new/order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ microservices-new/order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.UUID;
 
 import com.example.orderservice.dto.InventoryResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,9 +24,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
     final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -32,23 +37,30 @@ public class OrderService {
 
         List<String> skuCodes = order.getOrderItens().stream().map(OrderItems::getSkuCode).toList();
 
-        //Faz chamada inventory server, and cria order caso produto
-        //tenha em estoque
-        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
-        assert inventoryResponses != null;
-        boolean allProductsInStock;
-        allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
-        if(allProductsInStock){
-            orderRepository.save(order);
-            return "Ordem createad!";
-        } else {
-            throw new IllegalArgumentException("Produto não encontrado, por favor tente mais tarde!");
+        log.info("Caling inventory servuce");
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())){
+            //Faz chamada inventory server, and cria order caso produto
+            //tenha em estoque
+            InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+            assert inventoryResponses != null;
+            boolean allProductsInStock;
+            allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+            if(allProductsInStock){
+                orderRepository.save(order);
+                return "Ordem createad!!!!";
+            } else {
+                throw new IllegalArgumentException("Produto não encontrado, por favor tente mais tarde!");
+            }
+        } finally {
+            inventoryServiceLookup.end();
         }
+
 
     }
     private OrderItems mapToDto(OrderItemsDto ordemItemDto) {
